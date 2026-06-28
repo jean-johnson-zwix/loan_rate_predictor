4ccd296a2a17999f0d9b61b7ca2e04904384bd22
Jean Johnson <jajoh151@asu.edu>
2026-06-28 15:57:41 -0700

    feat: rever model package group description

--- infra/model_package_group.tf
+++ infra/model_package_group.tf
@@ -1,6 +1,6 @@
 resource "aws_sagemaker_model_package_group" "loan_rate_predictor" {
   model_package_group_name        = "loan-rate-predictor"
-  model_package_group_description = "XGBoost models for rate_spread prediction, versioned by training year"
+  model_package_group_description = "XGBoost models for rate_spread prediction, versioned by training vintage"
 
   tags = {
     Project = "loan-rate-predictor"
